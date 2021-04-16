cef94ad108d77eb9435188648024f662b0e2acd5
David E. Flores <floresdavide0@gmail.com>
2021-04-16 16:22:16 -0600

    Se creo el metodo 'SwitchChar' y se puso en función

--- src/Ejemplos/EstructuraSwitchChar.java
+++ src/Ejemplos/EstructuraSwitchChar.java
@@ -9,12 +9,41 @@ public class EstructuraSwitchChar {
         try{
             Scanner leer = new Scanner(System.in);
             
+            System.out.println("Ingrese el estado civil de la persona.");
+            String estadoCivil = leer.nextLine();
             
-            
-            
+            SwitchChar(estadoCivil.toUpperCase().charAt(0));
         }catch(Exception e){
             
             System.out.println(e.getMessage());
         }
+    }
+    
+    public static void SwitchChar(char estadoCivil){
+    
+        String mensaje;
+        
+        switch(estadoCivil){
+            case 'S':
+                mensaje = "Soltero/a";
+                break;
+            case 'C':
+                mensaje = "Casado/a";
+                break;
+            case 'D':
+                mensaje = "Divorsiado/a";
+                break;
+            case 'A':
+                mensaje = "Acompañado/a";
+                break;
+            case 'F':
+                mensaje = "Forever Alone";
+                break;
+            default:
+                mensaje = "Estado civil erroneo";
+                break;
+        }
+        
+        System.out.println("Su estado civil es: " + mensaje);
     }
 }
