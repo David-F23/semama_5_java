fbcfb1ecac63938caac4aa64ac3a8d53405791ea
David E. Flores <floresdavide0@gmail.com>
2021-04-16 16:56:45 -0600

    Se agrego el controlador de excepciones, se hizo instancia de la clase Scanner, se creo el metodo 'SwitchInt y se puso en función'

--- src/Ejemplos/EstructuraSwitchInt.java
+++ src/Ejemplos/EstructuraSwitchInt.java
@@ -1,6 +1,49 @@
 package Ejemplos;
 
+import java.util.Scanner;
+
 public class EstructuraSwitchInt {
     
+    public static void main(String[] args) {
+        
+        try{
+            Scanner leer = new Scanner(System.in);
+            
+            System.out.println("Ingrese un numero del 1 al 5");
+            int numero = leer.nextInt();
+            
+            SwitchInt(numero);
+        }catch(Exception e){
+            
+            System.out.println(e.getMessage());
+        }
+    }
     
+    public static void SwitchInt(int numero){
+        
+        String mensaje;
+        
+        switch(numero){
+            case 1:
+                mensaje = "UNO";
+                break;
+            case 2:
+                mensaje = "DOS";
+                break;
+            case 3:
+                mensaje = "TRES";
+                break;
+            case 4:
+                mensaje = "CUATRO";
+                break;
+            case 5:
+                mensaje = "CINCO";
+                break;
+            default:
+                mensaje = "Número no válido";
+                break;
+        }
+        
+        System.out.println("El numero es: " + mensaje);
+    }
 }
