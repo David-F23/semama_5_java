743932e8013d89961b4f29bf2e12f2352f2c28bc
David E. Flores <floresdavide0@gmail.com>
2021-04-15 21:01:08 -0600

    Se creo el metodo 'ifSencillo' y se puso en función

--- src/Ejemplos/EstructuraIfSencilla.java
+++ src/Ejemplos/EstructuraIfSencilla.java
@@ -9,10 +9,20 @@ public class EstructuraIfSencilla {
         try{
             Scanner leer = new Scanner(System.in); 
             
+            System.out.println("Ingrese la cantidad de su salario");
+            double salario = leer.nextDouble();
             
+            ifSencillo(salario);
         }catch(Exception e){
             
             System.out.println(e.getMessage());
+        }
+    }
+    
+    public static void ifSencillo(double salario){
+        
+        if(salario > 235){
+            System.out.println("Usted gana mas del salario mínimo");
         }
     }
 }
