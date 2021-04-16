0744e129e10b13ca94e28722ff4bed37b8e4c8c6
David E. Flores <floresdavide0@gmail.com>
2021-04-16 16:07:36 -0600

    Se agrego el controlador de excepciones y se hizo instancia de la clase scanner en la clase EstructuraSwitchChar

--- src/Ejemplos/EstructuraSwitchChar.java
+++ src/Ejemplos/EstructuraSwitchChar.java
@@ -1,5 +1,20 @@
 package Ejemplos;
 
+import java.util.Scanner;
+
 public class EstructuraSwitchChar {
     
+    public static void main(String[] args) {
+        
+        try{
+            Scanner leer = new Scanner(System.in);
+            
+            
+            
+            
+        }catch(Exception e){
+            
+            System.out.println(e.getMessage());
+        }
+    }
 }
