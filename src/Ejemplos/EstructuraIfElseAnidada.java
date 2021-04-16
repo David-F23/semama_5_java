6cd05b5655792490e17415f45cd4737674917489
David E. Flores <floresdavide0@gmail.com>
2021-04-16 15:50:46 -0600

    Se creo el metodo 'IfElseAnidada' y se puso en función

--- src/Ejemplos/EstructuraIfElseAnidada.java
+++ src/Ejemplos/EstructuraIfElseAnidada.java
@@ -10,10 +10,29 @@ public class EstructuraIfElseAnidada {
         
         try{
             
+            System.out.println("Ingrese la cantidad de su salario.");
+            double salario = leer.nextDouble();
             
+            IfElseAnidada(salario);
         }catch(Exception e){
             
             System.out.println(e.getMessage());
+        }
+    }
+    
+    public static void IfElseAnidada(double salario){
+        
+        if(salario > 235){
+            
+            System.out.println("Usted ganas mas del salario minimo");
+            
+        }else if(salario > 0){
+            
+            System.out.println("Usted gana menos del salario minimo");
+            
+        }else{
+            
+            System.out.println("Ingreso de dato erroneo");
         }
     }
 }
