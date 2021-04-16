e60daa59c70737363682c57032440d897c02a052
David E. Flores <floresdavide0@gmail.com>
2021-04-15 21:31:34 -0600

    Se creo el metodo 'ifElse' y se puso en funcionamiento

--- src/Ejemplos/EstructuraIfElse.java
+++ src/Ejemplos/EstructuraIfElse.java
@@ -9,9 +9,25 @@ public class EstructuraIfElse {
         try{
             
             Scanner leer = new Scanner(System.in);
+            
+            System.out.println("Ingrese la cantidad de su salario");
+            double salario = leer.nextDouble();
+            
+            ifElse(salario);
         }catch(Exception e){
             
             System.out.println(e.getMessage());
+        }
+    }
+    
+    public static void ifElse(double salario){
+        
+        if(salario > 235){
+            
+            System.out.println("Usted ganas mas del salario minimo");
+        }else{
+            
+            System.out.println("Usted gana menos del salario minimo");
         }
     }
 }
