722b37ad03fc4dde40a9ed407e82bdb75ae35fda
David E. Flores <floresdavide0@gmail.com>
2021-04-17 15:16:52 -0600

    En la clase 'EstructuraWhile' se utilizo el controlador de excepciones, se hizo instancia de la clase 'Scanner', se creo el metodo 'WhileSencillo' y se puso en funcionamiento

--- src/Ejemplos/EstructuraWhile.java
+++ src/Ejemplos/EstructuraWhile.java
@@ -1,14 +1,39 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Ejemplos;
 
-/**
- *
- * @author HP
- */
+import java.util.Scanner;
+
 public class EstructuraWhile {
     
+    public static void main(String[] args) {
+        
+        try{
+            Scanner leer = new Scanner(System.in);
+            
+            System.out.println("Ingresar un numero limite de repeticion");
+            String limite = leer.next();
+            
+            if(limite == null){
+                
+                System.out.println("Dato inválido");
+                
+            }else{
+                
+                WhileSencillo(Integer.parseInt(limite));
+            }
+        }catch(Exception e){
+            
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    public static void WhileSencillo(int limite){
+        
+        int i = 1;
+        
+        while(i <= limite){
+            
+            System.out.println("El valor de la repetición es: " + i);
+            i++;
+        }
+    }
 }
