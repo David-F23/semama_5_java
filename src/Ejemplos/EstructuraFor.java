3080998dfd26a226c15ac3fcffb41a8cf17711e3
David E. Flores <floresdavide0@gmail.com>
2021-04-17 15:32:47 -0600

    Se creo el metodo 'InstruccionFor' y se puso en funcionamiento

--- src/Ejemplos/EstructuraFor.java
+++ src/Ejemplos/EstructuraFor.java
@@ -1,14 +1,30 @@
 package Ejemplos;
 
+import java.util.Scanner;
+
 public class EstructuraFor {
     
     public static void main(String[] args) {
         
         try{
+            Scanner leer = new Scanner(System.in);
+            
+            System.out.println("Ingrese un numero limite de repetición");
+            String limite = leer.next();
+            
+            InstruccionFor(Integer.parseInt(limite));
             
         }catch(Exception e){
             
             System.out.println(e.getMessage());
+        }
+    }
+    
+    public static void InstruccionFor(int limite){
+        
+        for(int i = 1; i <= limite; i++){
+            
+            System.out.println("El valor de la repetición es: " + i);
         }
     }
 }
