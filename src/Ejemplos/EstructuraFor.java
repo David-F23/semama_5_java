2db374419b885bd6b2127d2cad861e4901bc8f10
David E. Flores <floresdavide0@gmail.com>
2021-04-17 15:25:52 -0600

    Se utilizo el controlador de excepciones

--- src/Ejemplos/EstructuraFor.java
+++ src/Ejemplos/EstructuraFor.java
@@ -4,5 +4,11 @@ public class EstructuraFor {
     
     public static void main(String[] args) {
         
+        try{
+            
+        }catch(Exception e){
+            
+            System.out.println(e.getMessage());
+        }
     }
 }
