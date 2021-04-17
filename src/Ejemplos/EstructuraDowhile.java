183d5d583b5e240d1e7e41dc7b96a7e0db809bde
David E. Flores <floresdavide0@gmail.com>
2021-04-17 15:46:15 -0600

    Se utilizó el controlador de excepciones, se creo el método 'InstruccionDowhile' y se puso en funcionamiento

--- src/Ejemplos/EstructuraDowhile.java
+++ src/Ejemplos/EstructuraDowhile.java
@@ -1,14 +1,32 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Ejemplos;
 
-/**
- *
- * @author HP
- */
+import java.util.Scanner;
+
 public class EstructuraDowhile {
     
+    public static void main(String[] args) {
+        
+        try{
+            Scanner leer = new Scanner(System.in);
+            
+            System.out.println("Ingrese un numero limite de repetición");
+            String limite = leer.next();
+            
+            InstruccionDowhile(Integer.parseInt(limite));
+            
+        }catch(Exception e){
+            
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    public static void InstruccionDowhile(int limite){
+            
+       int i = 1;
+       
+       do{
+           System.out.println("El valor de la repetición es: " + i);
+           i++;
+       }while(i <= limite);
+    }
 }
