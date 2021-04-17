c3ea15e05d495b88ca52f8656a7e020ab1fd18fe
David E. Flores <floresdavide0@gmail.com>
2021-04-17 10:29:04 -0600

    Se agrego el controlador de excepciones y se hizo instancia de la clase Scanner en la clase ejercicio2

--- src/Ejercicios/Ejercicio2.java
+++ src/Ejercicios/Ejercicio2.java
@@ -1,9 +1,18 @@
 package Ejercicios;
 
+import java.util.Scanner;
+
 public class Ejercicio2 {
     
     public static void main(String[] args) {
         
-        
+        try{
+            Scanner leer = new Scanner(System.in);
+            
+            
+        }catch(Exception e){
+            
+            System.out.println(e.getMessage());
+        }
     }
 }
