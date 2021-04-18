4ea86b010e5ad21d798274c300afa009bf5ce96b
David E. Flores <floresdavide0@gmail.com>
2021-04-18 09:18:50 -0600

    Se creo el método 'DeprecMaquinaria' y se puso en funcionamiento

--- src/Ejercicios/Ejercicio4.java
+++ src/Ejercicios/Ejercicio4.java
@@ -1,5 +1,6 @@
 package Ejercicios;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Ejercicio4 {
@@ -9,11 +10,39 @@ public class Ejercicio4 {
         try{
             Scanner leer = new Scanner(System.in);
             
+            System.out.println("INGRESE LOS SIGUIENTES DATOS.");
+            System.out.println("=============================");
+            System.out.println("Ingrese el nombre de la maquina");
+            String nombre = leer.nextLine();
+            System.out.println("Ingrese el costo de la maquinaria");
+            double costo = leer.nextDouble();
+            System.out.println("Ingrese los años de la depreciaición");
+            int anios = leer.nextInt();
+            System.out.println("====================================");
             
+            DeprecMaquinaria(costo, anios);
             
         }catch(Exception e){
             
             System.out.println(e.getMessage());
+        }
+    }
+    
+    public static void DeprecMaquinaria(double costo, int anios){
+        
+        DecimalFormat formato = new DecimalFormat("#.####");
+        
+        double depreciacion = anios * (anios + 1) / 2;
+        int i = 1;
+        while(i <= anios ){
+            
+            double deprecAnio = 100 * (anios / depreciacion);
+            double totalDeprec = costo * (anios / depreciacion);
+            
+            System.out.println("La de preciación en el año " + anios + " es de: $" + formato.format(deprecAnio) + "%");
+            System.out.println("EL valor es de: " + totalDeprec);
+            System.out.println("================================================");
+            anios--;
         }
     }
 }
