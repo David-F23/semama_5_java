91428ee49bf535bbbe86913e2b9c002da6bad1e4
David E. Flores <floresdavide0@gmail.com>
2021-04-18 08:21:26 -0600

    Se agrego el control de excepciones y se hizo instancia de la clase Scanner

--- src/Ejercicios/Ejercicio4.java
+++ src/Ejercicios/Ejercicio4.java
@@ -1,14 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Ejercicios;
 
-/**
- *
- * @author HP
- */
+import java.util.Scanner;
+
 public class Ejercicio4 {
     
+    public static void main(String[] args) {
+        
+        try{
+            Scanner leer = new Scanner(System.in);
+            
+            
+            
+        }catch(Exception e){
+            
+            System.out.println(e.getMessage());
+        }
+    }
 }
