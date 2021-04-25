85114d2ed6b16cc33b07c498d4c7050f88aa3851
David E. Flores <floresdavide0@gmail.com>
2021-04-25 10:34:13 -0600

    Modificación

--- src/Ejercicios/Ejercicio5.java
+++ src/Ejercicios/Ejercicio5.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Ejercicio5 {
     
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         
         try{
             Scanner leer = new Scanner(System.in);
@@ -12,15 +12,18 @@ public class Ejercicio5 {
             System.out.println("Ingrese los minutos que duró la llamada");
             int minutos = leer.nextInt();
             
-            CalcularLlamada(minutos);
+            System.out.println(CalcularLlamada(minutos));
+            
+            System.out.println("================");
+            
         }catch(Exception e){
             
             System.out.println(e.getMessage());
         }
     }
     
-    public static void CalcularLlamada(int tiempo){
-        
+    public static double CalcularLlamada(int tiempo){
+        double total = 0.0;
         double costoBase = 0.20;
         double totalFor = 0.0;
         
@@ -37,10 +40,61 @@ public class Ejercicio5 {
                 
             }
             
-            double total = costoBase + totalFor;
+            total = costoBase + totalFor;
             
-            
-            System.out.println("El total es: $" + total);
         }
+        
+        return total;
+    }*/
+    
+    public double tiempo;
+
+    public double getTiempo() {
+        return tiempo;
+    }
+
+    public void setTiempo(double tiempo) {
+        this.tiempo = tiempo;
+    }
+    
+    public double precio_llamada(double tiempo_minutos){
+        double costoLlamada = 0;
+        double minutos_extra = 0;
+        double pagoxminExtras = 0;
+        if(tiempo_minutos>3){
+            minutos_extra = tiempo_minutos - 3;  //Minutos extra
+        }else{
+            minutos_extra = 0;
+        }
+        pagoxminExtras = minutos_extra * 0.5;    //Centavos
+        costoLlamada = 0.2 + pagoxminExtras ;
+        return costoLlamada;
+    }
+    
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        Ejercicio5 monto = new Ejercicio5();
+        
+        String[] vector;
+        
+        System.out.println("Ingrese el tiempo de duración de la llamada");
+        monto.setTiempo(Double.parseDouble(leer.nextLine()));
+        double dinero = monto.precio_llamada(monto.getTiempo());
+        //conversion del dinero a String.
+        String pisto = String.valueOf(dinero);
+        
+        vector = new String[pisto.length()];
+        
+        for (int i = 0; i < pisto.length(); i++) {
+            vector[i] =  pisto.substring(i);
+        }
+        
+        System.out.println("*******************************");
+        
+        for (int i = 0; i < vector.length; i++) {
+            System.out.println(vector[i]);
+        }
+        
+        System.out.println("El precio por su llamada es: $" + monto.precio_llamada(monto.getTiempo()));
     }
 }
