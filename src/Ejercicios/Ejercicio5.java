668fdeb0f8cb00d3a8a952a8345a583fd9aa9f92
David E. Flores <floresdavide0@gmail.com>
2021-04-25 14:41:23 -0600

    Se creo el método 'Total' y se puso en funcionamiento

--- src/Ejercicios/Ejercicio5.java
+++ src/Ejercicios/Ejercicio5.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Ejercicio5 {
     
-    /*public static void main(String[] args) {
+    public static void main(String[] args) {
         
         try{
             Scanner leer = new Scanner(System.in);
@@ -12,9 +12,12 @@ public class Ejercicio5 {
             System.out.println("Ingrese los minutos que duró la llamada");
             int minutos = leer.nextInt();
             
+            System.out.println("El costo de su llamada es de: $");
             System.out.println(CalcularLlamada(minutos));
             
-            System.out.println("================");
+            System.out.println("================\n En total:");
+            
+            Total(CalcularLlamada(minutos));
             
         }catch(Exception e){
             
@@ -45,9 +48,40 @@ public class Ejercicio5 {
         }
         
         return total;
-    }*/
+    }
     
-    public double tiempo;
+    public static void Total(double total){
+        
+        double billete_1, billete_5, billete_10, billete_20;
+        double moneda_25, moneda_10, moneda_5, moneda_1;
+        
+        moneda_1 = total;
+        billete_20 = (moneda_1 - moneda_1 % 20) / 20;
+        moneda_1 = moneda_1 % 20;
+        billete_10 = (moneda_1 - moneda_1 % 10) / 10;
+        moneda_1 = moneda_1 % 10;
+        billete_5 = (moneda_1 - moneda_1 % 5) / 5;
+        moneda_1 = moneda_1 % 10;
+        billete_1 = (moneda_1 - moneda_1 % 1) / 1;
+        moneda_1 = moneda_1 % 1;
+        moneda_25 = (moneda_1 - moneda_1 % 0.25) / 0.25;
+        moneda_1 = moneda_1 % 0.25;
+        moneda_10 = (moneda_1 - moneda_1 % 0.10) / 0.10;
+        moneda_1 = moneda_1 % 0.10;
+        moneda_5 = (moneda_1 - moneda_1 % 0.05) / 0.05;
+        moneda_1 = moneda_1 % 0.05;
+        
+        System.out.println("Billetes de 20 => " + (int)billete_20);
+        System.out.println("Billetes de 10 => " + (int)billete_10);
+        System.out.println("Billetes de 5 => " + (int)billete_5);
+        System.out.println("Billetes de 1 => " + (int)billete_1);
+        System.out.println("Monedas de 0.25 => " + (int)moneda_25);
+        System.out.println("Monedas de 0.10 => " + (int)moneda_10);
+        System.out.println("Monedas de 0.05 => " + (int)moneda_5);
+        System.out.println("Monedas de 0.01 => " + (int)moneda_1);
+    }
+    
+    /*public double tiempo;
 
     public double getTiempo() {
         return tiempo;
@@ -96,5 +130,5 @@ public class Ejercicio5 {
         }
         
         System.out.println("El precio por su llamada es: $" + monto.precio_llamada(monto.getTiempo()));
-    }
+    }*/
 }
